bcaedfd6c510eee5fd0b12d7035afb70b97d859a
kurozakizz <squallcope@gmail.com>
2017-12-24 10:47:11 +0700

    [ADD] Create LoginController that pass acceptance test

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        // POST api/login
+        [HttpPost]
+        public System.Dynamic.ExpandoObject Post([FromBody]string value)
+        {
+            dynamic ployUser = new System.Dynamic.ExpandoObject();
+            ployUser.id = 1;
+            ployUser.username = "ploy";
+            ployUser.displayname = "พลอย";
+            return ployUser;
+        }
+    }
+}
